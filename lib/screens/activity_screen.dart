ad5cb772cc649ae0ec5a25804bfe87e02850e7a4
Henry <pienry3g@gmail.com>
2019-10-29 09:45:13 +0100

    day7: part 1: (activity screen) implement the activity screeen
    create the activity screen
    - create the single activity widget

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_app/widgets/composedWidgets/activity/single_activity.dart';
 import 'package:instagram_app/widgets/composedWidgets/bottom_nav_bar.dart';
 import 'package:instagram_app/widgets/composedWidgets/top_nav_bar.dart';
 
 class ActivityScreen extends StatelessWidget {
   static final routeName = '/activity';
+
+  _getActivities() => ListView.builder(
+        itemCount: 26,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'This Week',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            );
+          }
+          if (index == 6) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'This Month',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            );
+          }
+          if (index == 16) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Earlier',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            );
+          }
+          return SingleActivity();
+        },
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: tonNavBar(title: Text('Activity'), actions: <Widget>[], leadingActions: null),
-      body: Container(),
+      appBar: tonNavBar(
+          title: Text('Activity'), actions: <Widget>[], leadingActions: null),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: _getActivities(),
+      ),
       bottomNavigationBar: bottomNavBar(context),
     );
   }
